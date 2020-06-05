46f750d6fd8651eddf79aa831fbfec7c4eab6e19
Sudha <laxmsudha_m@hotmail.com>
2020-06-05 23:30:25 +0000

    Restored file

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
-//This is Java program will print a sample message
 class Hello{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");
- 
-     System.out.println("Test");  
+     System.out.println("Hello Java");  
     }  
 }  
